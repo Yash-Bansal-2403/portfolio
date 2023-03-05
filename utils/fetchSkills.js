8b20d46c0089a916a381ac185314552ad7bf0e28
Yash-Bansal-2403 <ybansal2403@gmail.com>
2023-03-05 12:03:47 +0530

    url changed

--- utils/fetchSkills.js
+++ utils/fetchSkills.js
@@ -1,7 +1,7 @@
 
 
 export const fetchSkills = async() => {
-    const res = await fetch(`https://localhost:3000//api/getSkills`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
 
     const data = await res.json()
     const skills= data.skills;
